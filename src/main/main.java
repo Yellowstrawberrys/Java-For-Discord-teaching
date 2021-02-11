16e91f471fb6c92f3628fcae5f0b60c5156269c9
Yellowstrawberrys <77413533+Yellowstrawberrys@users.noreply.github.com>
2021-02-12 00:40:39 +0100

    Create main.java

---
+++ src/main/main.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.Scanner;
+public class main{
+  public static void main(String[] args){
+    System.out.println("asdfas");
+    Scanner test = new Scanner(System.in);
+    if (test.nextLine().equals("test")){
+      System.out.println("테스트 커맨드 입니다");
+    }
+  }
+}
